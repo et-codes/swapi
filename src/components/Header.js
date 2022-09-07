2a7cfd9ed4dcdc9f2a8652b8c640f5558ef16a33
Eric Thornton <ericthornton43@gmail.com>
2022-09-07 14:18:02 -0600

    Add Star Wars and GitHub logos.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import swLogo from '../assets/sw-logo.svg';
+import gitHubLogo from '../assets/github-logo.png';
+
 const Header = () => {
   return (
-    <h1 className="my-3">
-      Star Wars Character Search
-    </h1>
+    <div className="d-flex align-items-center justify-content-center">
+      <img src={swLogo} alt="Star Wars logo" width={'25%'} />
+      <h1 className="me-auto ms-1">Character Search</h1>
+      <a
+        href='https://github.com/et-codes/swapi'
+        target='_blank'
+        rel="noreferrer noopener"
+      >
+        <img src={gitHubLogo} alt="GitHub logo" />
+      </a>
+    </div>
   );
 }
 
